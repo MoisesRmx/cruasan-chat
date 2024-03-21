21fe89a17b06d774e62dadf445c1cb3f42c0c3ce
Moises Roberto <moisesrobertosanchezperez@gmail.com>
2024-03-21 16:48:43 -0600

    jaklñjdlñfs

--- src/controllers/router.controller.js
+++ src/controllers/router.controller.js
@@ -23,7 +23,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/clave', (req, res) => {
-  res.send(process.env.SECRET, process.env.DB_URLCN)
+  res.send(process.env.SECRET)
 })
 
 // router.post('/register', AuthUsers.register)
